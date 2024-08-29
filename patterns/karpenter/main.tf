ce40b4dee5cbeb81546a9886b756948a67eb6490
vchintal <vijay.chintalapati@gmail.com>
2024-08-29 13:46:47 -0700

    Removed the kubernetes group line which doesn't apply to a Node role

--- patterns/karpenter/main.tf
+++ patterns/karpenter/main.tf
@@ -165,10 +165,9 @@ module "eks_blueprints_addons" {
 }
 
 resource "aws_eks_access_entry" "karpenter_node_access_entry" {
-  cluster_name      = module.eks.cluster_name
-  principal_arn     = module.eks_blueprints_addons.karpenter.node_iam_role_arn
-  kubernetes_groups = []
-  type              = "EC2_LINUX"
+  cluster_name  = module.eks.cluster_name
+  principal_arn = module.eks_blueprints_addons.karpenter.node_iam_role_arn
+  type          = "EC2_LINUX"
 }
 
 ################################################################################
